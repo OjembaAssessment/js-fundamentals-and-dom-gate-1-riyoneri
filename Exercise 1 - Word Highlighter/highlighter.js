7f9f0648dbe34bea8dd058688696f22d78ecf341
Lionel Kaneza <lionkaneza@gmail.com>
2023-07-24 10:25:55 +0200

    add links to exercise 1

--- Exercise 1 - Word Highlighter/highlighter.js
+++ Exercise 1 - Word Highlighter/highlighter.js
@@ -1,17 +1,17 @@
-// console.log("Exercise 1 - Word Highlighter");
-// const para = document.getElementById("myParagraph")
-// var wordCounts = {};
-// var words = para.textContent.split(/\b/);
+console.log("Exercise 1 - Word Highlighter");
+const para = document.getElementById("myParagraph")
+var wordCounts = {};
+var words = para.textContent.split(/\b/);
 
-// for (var i = 0; i < words.length; i++) {
-//     if (!(/[ ,.]/.test(words[i])))
-//         wordCounts[words[i]] = (wordCounts[words[i]] || 0) + 1
-// }
+for (var i = 0; i < words.length; i++) {
+    if (!(/[ ,.]/.test(words[i])))
+        wordCounts[words[i]] = (wordCounts[words[i]] || 0) + 1
+}
 
-// const sortedArr = [];
+const sortedArr = [];
 
-// for (let word in wordCounts) sortedArr.push([word, wordCounts[word]])
+for (let word in wordCounts) sortedArr.push([word, wordCounts[word]])
 
-// sortedArr.sort((a, b) => b[1] - a[1])
-// console.table(sortedArr)
-//     // console.table(wordCounts)
+sortedArr.sort((a, b) => b[1] - a[1])
+console.table(sortedArr)
+    // console.table(wordCounts)
