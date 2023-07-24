bcbd1e2b70eac7feb41dd25d484429cad6cd9a01
Lionel Kaneza <lionkaneza@gmail.com>
2023-07-24 10:23:21 +0200

    exercise 3 finish

--- Exercise 1 - Word Highlighter/highlighter.js
+++ Exercise 1 - Word Highlighter/highlighter.js
@@ -1,1 +1,17 @@
-console.log("Exercise 1 - Word Highlighter");
+// console.log("Exercise 1 - Word Highlighter");
+// const para = document.getElementById("myParagraph")
+// var wordCounts = {};
+// var words = para.textContent.split(/\b/);
+
+// for (var i = 0; i < words.length; i++) {
+//     if (!(/[ ,.]/.test(words[i])))
+//         wordCounts[words[i]] = (wordCounts[words[i]] || 0) + 1
+// }
+
+// const sortedArr = [];
+
+// for (let word in wordCounts) sortedArr.push([word, wordCounts[word]])
+
+// sortedArr.sort((a, b) => b[1] - a[1])
+// console.table(sortedArr)
+//     // console.table(wordCounts)
--- Exercise 3 - Timer/timer.js
+++ Exercise 3 - Timer/timer.js
@@ -1,1 +1,54 @@
-console.log("Exercise 3 - Timer");
+const reduceBtn = document.getElementById("subtract")
+const addBtn = document.getElementById("add")
+
+const secondsHolder = document.getElementById("seconds")
+
+const resetBtn = document.getElementById("reset")
+const startBtn = document.getElementById("start")
+
+let timer;
+
+reduceBtn.addEventListener("click", () => {
+    if (timer) return;
+
+    let seconds = +secondsHolder.textContent.slice(0, secondsHolder.textContent.length - 1)
+    if (seconds <= 0) return;
+
+    secondsHolder.textContent = `${seconds -= 1}s`
+})
+
+addBtn.addEventListener("click", () => {
+    if (timer) return;
+
+    let seconds = +secondsHolder.textContent.slice(0, secondsHolder.textContent.length - 1)
+
+    secondsHolder.textContent = `${seconds += 1}s`
+})
+
+resetBtn.addEventListener("click", () => {
+    if (timer) return;
+
+    secondsHolder.textContent = '0s'
+})
+
+startBtn.addEventListener("click", () => {
+    let seconds = +secondsHolder.textContent.slice(0, secondsHolder.textContent.length - 1)
+    if (seconds <= 0) return;
+
+    if (timer) {
+        startBtn.textContent = "START"
+        clearInterval(timer)
+        return timer = null
+    }
+
+    startBtn.textContent = "STOP"
+    timer = setInterval(() => {
+        seconds--
+        secondsHolder.textContent = `${seconds}s`
+        if (seconds == 0) {
+            clearInterval(timer)
+            startBtn.textContent = "START"
+            timer = null;
+        }
+    }, 1000);
+})
